cc2b63caa4da16a403c58a1605bd5ecb7c0f8d8b
chibinaoko <tranghuyenttht@gmail.com>
2016-05-03 11:48:47 +0700

    Sign up

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  get 'users/new'
+
+  root 'static_pages#home'
+  get 'help' => 'static_pages#help'
+  get 'signup'  => 'users#new'
+  resources :users
+
+
+
+
+end
